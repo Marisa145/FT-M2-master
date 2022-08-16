756931aaf7de642ac7705d9dfc94361abdc0817c
Marisa145 <marisa0199914@gmail.comgit config --global user.email marisa0199914@gmail.comgit config --global user.name Marisa>
2022-08-16 00:47:41 -0300

    resuelto

--- 03-ES6/homework/src/selector.js
+++ 03-ES6/homework/src/selector.js
@@ -9,7 +9,14 @@ var traverseDomAndCollectElements = function(matchFunc, startEl) {
   // usa matchFunc para identificar elementos que matchien
 
   // TU CÓDIGO AQUÍ
-  
+  if(matchFunc(startEl)) resultSet.push(startEl);
+
+  for (let i = 0; i < startEl.children.length; i++) {
+    let child = startEl.children[i];
+    let collectedElements = traverseDomAndCollectElements(matchFunc, child);
+    resultSet = [...resultSet, ...collectedElements]
+  }
+  return resultSet;
 };
 
 // Detecta y devuelve el tipo de selector
@@ -18,6 +25,12 @@ var traverseDomAndCollectElements = function(matchFunc, startEl) {
 
 var selectorTypeMatcher = function(selector) {
   // tu código aquí
+  if(selector[0] === "#") return 'id';
+  else if(selector[0] ===  ".") return 'class';
+  else if(selector.split('.').length === 2) return 'tag.class';    // lo que hace el split es dividir en un arreglo mi string
+  else { return 'tag'}
+
+
   
 };
 
@@ -25,18 +38,32 @@ var selectorTypeMatcher = function(selector) {
 // recuerda, la función matchFunction devuelta toma un elemento como un
 // parametro y devuelve true/false dependiendo si el elemento
 // matchea el selector.
+// 3 tipos de class =  class=, className, o classList
 
 var matchFunctionMaker = function(selector) {
   var selectorType = selectorTypeMatcher(selector);
   var matchFunction;
   if (selectorType === "id") { 
-   
+   matchFunction = elements => "#" + elements.id === selector;
   } else if (selectorType === "class") {
-    
+    matchFunction= elements => {
+      let classes = elements.classList
+      for (let i = 0; i < classes.length; i++) {
+         if('.' + classes[i] === selector) return true
+      }
+         
+      return false
+        
+      
+    }
   } else if (selectorType === "tag.class") {
-    
+    matchFunction = elements => {
+      let [t, c] = selector.split('.'); 
+    return matchFunctionMaker(t) (elements)  &&  matchFunctionMaker('.' + c ) (elements);
+    }
+   
   } else if (selectorType === "tag") {
-    
+    matchFunction = elements => elements.tagName === selector.toUpperCase();
   }
   return matchFunction;
 };
