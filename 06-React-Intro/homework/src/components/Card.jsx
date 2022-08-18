f33847170f1618ea75839b60739107e26a6e582c
Marisa145 <marisa0199914@gmail.comgit config --global user.email marisa0199914@gmail.comgit config --global user.name Marisa>
2022-08-17 23:18:10 -0300

    resuelto

--- 06-React-Intro/homework/src/components/Card.jsx
+++ 06-React-Intro/homework/src/components/Card.jsx
@@ -2,5 +2,21 @@ import React from 'react';
 
 export default function Card(props) {
   // acá va tu código
-  return <div>Card Component</div>
+  return(
+    <div>
+      <button onClick={props.onClose}>X</button>
+      <h4>{props.name}</h4>
+      <div> 
+        <p>Min</p>
+        <p>{props.min}</p>
+        <p>Max</p>
+        <p>{props.max}</p>
+      </div>
+      <img src={`http://openweathermap.org/img/wn/${props.img}@2x.png`} alt={"img"}/>
+    </div>
+  )
+
 };
+
+// colocamos entre llaves codigo js --el texto no tinen que estar entre llaves
+// si la imagen no salta colocamos alt=img
--- 06-React-Intro/homework/src/components/Cards.jsx
+++ 06-React-Intro/homework/src/components/Cards.jsx
@@ -1,7 +1,31 @@
 import React from 'react';
+import Card from './Card';
 
 export default function Cards(props) {
   // acá va tu código
-  // tip, podés usar un map
-  return <div>Cards Component</div>
+  // tip, podés usar un map= buscamos que recorremos
+  if(!props.cities){
+    return <h1>Nohay ciudades disponibles</h1>
+  }
+
+  return (
+  <div>
+      {
+       props.cities && props.cities.map(city =>(
+        <Card
+        Key={city.id}
+        max={city.main.temp_max}
+          min={city.main.temp_min}
+          name={city.name}
+          img={city.weather[0].icon}
+          onClose={() => alert(city.name)}
+          />
+          
+       ))
+      }
+
+  </div>
+  
+  
+    )
 };
